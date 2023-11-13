6300b9957159845e6b9965e2a9e2d4a524a36a15
2314201 <23614201@stu.mmu.ac.uk>
2023-11-13 21:27:56 +0000

    Navigation

--- js/scripts.js
+++ js/scripts.js
@@ -57,10 +57,143 @@ window.addEventListener('DOMContentLoaded', event => {
     
 });
 
+ /* CAROUSEL https://github.com/Vetrivel-VP/expandable_carousel_flex/commit/ */
 
 $(".option").click(function () {
   $(".option").removeClass("active");
   $(this).addClass("active");
 });
-// adding listener event click - Remove all the active class from option and add the
-// active class for the current clicked option
+
+ /* CAROUSEL /*/
+
+
+/* NAVIGATION https://github.com/hakimel/css/tree/master/progress-nav */
+
+ window.onload = function() {
+
+	var toc = document.querySelector( '.toc' );
+	var tocPath = document.querySelector( '.toc-marker path' );
+	var tocItems;
+
+	// Factor of screen size that the element must cross
+	// before it's considered visible
+	var TOP_MARGIN = 0.1,
+		BOTTOM_MARGIN = 0.2;
+
+	var pathLength;
+
+	var lastPathStart,
+		lastPathEnd;
+
+	window.addEventListener( 'resize', drawPath, false );
+	window.addEventListener( 'scroll', sync, false );
+
+	drawPath();
+
+	function drawPath() {
+
+		tocItems = [].slice.call( toc.querySelectorAll( 'li' ) );
+
+		
+		tocItems = tocItems.map( function( item ) {
+			var anchor = item.querySelector( 'a' );
+			var target = document.getElementById( anchor.getAttribute( 'href' ).slice( 1 ) );
+
+			return {
+				listItem: item,
+				anchor: anchor,
+				target: target
+			};
+		} );
+
+		
+		tocItems = tocItems.filter( function( item ) {
+			return !!item.target;
+		} );
+
+		var path = [];
+		var pathIndent;
+
+		tocItems.forEach( function( item, i ) {
+
+			var x = item.anchor.offsetLeft - 5,
+				y = item.anchor.offsetTop,
+				height = item.anchor.offsetHeight;
+
+			if( i === 0 ) {
+				path.push( 'M', x, y, 'L', x, y + height );
+				item.pathStart = 0;
+			}
+			else {
+				
+				if( pathIndent !== x ) path.push( 'L', pathIndent, y );
+
+				path.push( 'L', x, y );
+
+				
+				tocPath.setAttribute( 'd', path.join( ' ' ) );
+				item.pathStart = tocPath.getTotalLength() || 0;
+
+				path.push( 'L', x, y + height );
+			}
+
+			pathIndent = x;
+
+			tocPath.setAttribute( 'd', path.join( ' ' ) );
+			item.pathEnd = tocPath.getTotalLength();
+
+		} );
+
+		pathLength = tocPath.getTotalLength();
+
+		sync();
+
+	}
+
+	function sync() {
+
+		var windowHeight = window.innerHeight;
+
+		var pathStart = pathLength,
+			pathEnd = 0;
+
+		var visibleItems = 0;
+
+		tocItems.forEach( function( item ) {
+
+			var targetBounds = item.target.getBoundingClientRect();
+
+			if( targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * ( 1 - BOTTOM_MARGIN ) ) {
+				pathStart = Math.min( item.pathStart, pathStart );
+				pathEnd = Math.max( item.pathEnd, pathEnd );
+
+				visibleItems += 1;
+
+				item.listItem.classList.add( 'visible' );
+			}
+			else {
+				item.listItem.classList.remove( 'visible' );
+			}
+
+		} );
+
+
+		if( visibleItems > 0 && pathStart < pathEnd ) {
+			if( pathStart !== lastPathStart || pathEnd !== lastPathEnd ) {
+				tocPath.setAttribute( 'stroke-dashoffset', '1' );
+				tocPath.setAttribute( 'stroke-dasharray', '1, '+ pathStart +', '+ ( pathEnd - pathStart ) +', ' + pathLength );
+				tocPath.setAttribute( 'opacity', 1 );
+			}
+		}
+		else {
+			tocPath.setAttribute( 'opacity', 0 );
+		}
+
+		lastPathStart = pathStart;
+		lastPathEnd = pathEnd;
+
+	}
+
+};
+
+/* NAVIGATION */
